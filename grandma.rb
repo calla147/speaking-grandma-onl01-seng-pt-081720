15eeb0a7e4603a79e8f5eff296d42ff3735afc3c
noella callahan <calla147@gmail.com>
2020-08-30 01:55:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
   if string != string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
-    return "NO, NOT SINCE 1938!"
+    return "H!"
+  elsif string.upcase == string.upcase
+    return ""
   end
+end
